ba3635db6e2a061b525210292e92cfcc9cfc343b
OakenMan <tom.suchel@gmail.com>
2020-09-25 11:51:22 +0200

    update

--- tp3_login.py
+++ tp3_login.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter.messagebox import showinfo
 import hashlib
+import os
 
 master = tk.Tk()
 
@@ -24,20 +25,53 @@ buttonLogIn = tk.Button(master, text="Log In", width=15, command=lambda: logIn()
 buttonLogIn.pack()
 
 def signUp():
-    hash = hashlib.sha512(entryPassword.get().encode()).hexdigest()
-    with open("./infos.txt", "w") as file:
-        file.write(entryLogin.get()+"\n"+hash)
+
+    # On génère le sel
+    salt = os.urandom(32)
+
+    # On génère le hash
+    hash = hashlib.pbkdf2_hmac(
+        'sha256',                               # Algo à utiliser
+        entryPassword.get().encode('utf-8'),    # On convertit en bytes
+        salt,                                   # On lui fournit le "sel"
+        100000                                  # Nbr. d'itérations de l'algo
+    )
+
+    # On écrit le login, sel et hash dans un fichier binaire sous cette forme :
+    # 0: login
+    # 1: sel+hash
+    with open("./infos.txt", "wb") as file:
+        loginBytes = entryLogin.get().encode('utf-8')
+        file.write(loginBytes)
+        file.write('\n'.encode('utf-8'))
+        file.write(salt+hash)
         showinfo("Success", "Sign up successfull!")
-        
+
 
 def logIn():
-    hash = hashlib.sha512(entryPassword.get().encode()).hexdigest()
-    with open("./infos.txt", "r") as file:
+
+    with open("./infos.txt", "rb") as file:
         infos = file.readlines()
-        trueHash = infos[1]
-        if trueHash == hash:
+
+        # On récupère le salt et le hash de l'utilisateur
+        login = infos[0][:-1]
+        salt = infos[1][:32]
+        hash = infos[1][32:]
+
+        # On recréé le nouveau hash de la même façon qu'on a créé l'ancien
+        currentHash = hashlib.pbkdf2_hmac(
+            'sha256',
+            entryPassword.get().encode('utf-8'),
+            salt,
+            100000
+        )
+
+        currentLogin = entryLogin.get().encode('utf-8')
+
+        # Si les 2 hashs (et les 2 logins) sont identiques, c'est OK!
+        if currentHash == hash and currentLogin == login:
             showinfo("Success", "Login successfull !")
         else:
-            showinfo("Error", "Password incorrect !")
+            showinfo("Error", "Login or password incorrect !")
 
 master.mainloop()
